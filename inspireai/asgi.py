8ca3683710eb785a88261b1d8852ad369c106665
Mateo Lopera Ortiz <mloperao1@eafit.edu.co>
2025-04-20 04:33:04 +0000

    code syntax for the chat

--- inspireai/asgi.py
+++ inspireai/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspireai.settings')
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 import interaction.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             interaction.routing.websocket_urlpatterns
